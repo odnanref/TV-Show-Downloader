fbc68f48bfcafc99fea91453ada99b6a82923b4f
Fernando Andre <netriver@gmail.com>
2013-04-26 15:19:39 -0300

    listeners file to be inserted into the dispatcher

---
+++ EventListeners.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# ADD HERE Your event Listeners
+#
+
+from DownloadEvent import DownloadEvent
+from MailerListener import MailerListener
+
+class EventListeners(object):
+    Listeners = {}
+
+    def __init__(self, ConfManager):
+        DL = MailerListener()
+        DL.setConfig(ConfManager)
+        self.Listeners[DownloadEvent.DOWNLOAD_READY] = DL.update
+
+    def add(self, TYPE, EventListener):
+        self.Listeners[TYPE] = EventListener
+
+    def getAll(self):
+        return self.Listeners
+
